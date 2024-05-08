d16c5456e01d671d2bd91a5fdb7f581cba3a8a5c
FiraoolTulu <117259416+firaoltulu@users.noreply.github.com>
2024-05-08 22:15:03 +0300

    my second commit

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
-
-"""Task 0."""
+""" BaseCaching module."""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """This class `BasicCache` that inherits from `BaseCaching`
-    and is a caching system.
+    """
+    Defines a class for caching information in key-value pairs
+    Methods:
+        put(key, item) - store a key-value pair
+        get(key) - retrieve the value associated with a key
     """
 
+    def __init__(self):
+        """this method Initialize the class using the parent class __init__ method."""
+        BaseCaching.__init__(self)
+
     def put(self, key, item):
-        """This Method assign to the dictionary `self.cache_data` the
-        `item` value for the key `key`."""
-        if key is not None and item is not None:
+        """this method Store a key-value pair.
+        Args:
+            Key
+            Item
+        """
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """THis method return the value in `self.cache_data` linked to `key`."""
-        return self.cache_data.get(key, None)
+        """this method Return value linked to key."""
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
